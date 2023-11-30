1a888a86d8d34b5dd4315c1ca81cd4953fa05682
guilherme rabelo <guilherme.rabelo@matriz.fornecedoramaquinas.com.br>
2023-11-30 09:46:07 -0300

    Feat/Criando funcao de login no ERP Dealernet

---
+++ src/login.py
@@ -0,0 +1,20 @@
+from pywinauto.application import Application
+import pyautogui
+import time
+import subprocess
+
+def loginDealernet(modulo, senha):
+    comando_powershell = f"start {modulo}"
+    subprocess.run(["powershell", "-Command", comando_powershell], capture_output=True, text=True)
+    time.sleep(5)
+
+    app = Application(backend="win32").connect(title='Segurança')
+    time.sleep(2)
+    app.Seguranca.set_focus()
+
+    app.Seguranca.child_window(class_name="Edit").wrapper_object().click_input()
+    pyautogui.write(senha)
+    time.sleep(2)
+    app.Seguranca.child_window(title="&OK", class_name="Button").wrapper_object().click_input()
+    time.sleep(5)
+

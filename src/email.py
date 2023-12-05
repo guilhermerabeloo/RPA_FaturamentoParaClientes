69f2132398b0829c00b16bcb9a7f934f3ed65a2a
guilherme rabelo <guilherme.rabelo@matriz.fornecedoramaquinas.com.br>
2023-12-05 17:35:24 -0300

    Fix/Ajustando funcao de envio de email

--- src/email.py
+++ src/email.py
@@ -1,29 +1,32 @@
 import win32com.client as win32
 import os
 
-outlook = win32.Dispatch('outlook.application')
-email = outlook.CreateItem(0)
-
-
 html_file_path_carteira = os.path.join(os.path.dirname(__file__), 'assets', 'emailCarteira.html')
 html_file_path_boleto = os.path.join(os.path.dirname(__file__), 'assets', 'emailBoleto.html')
 
 def envioDoEmail(tipo, dados):
+    print('Iniciando envio do email do lancamento ', dados['titulo'])
+
+    outlook = win32.Dispatch('outlook.application')
+    email = outlook.CreateItem(0)
+
     if tipo == 'carteira':
         with open(html_file_path_carteira, 'r', encoding='utf-8') as file:
             html_body = file.read()
-            html_body = html_body.format(cliente=dados['cliente'], emissao=dados['emissao'], empresa=dados['empresa'], numeroNotaFiscal=dados['nota'], vencimentoNotaFiscal=dados['vencimento'], valorTitulo=dados['titulo'])
+            html_body = html_body.format(cliente=dados['cliente'], emissao=dados['emissao'], empresa=dados['empresa'], numeroNotaFiscal=dados['titulo'], vencimentoNotaFiscal=dados['vencimento'], valorBoleto=dados['boleto'], valorTitulo=dados['nota'])
         assunto = f"Olá {dados['cliente']}, a sua Nota Fiscal está disponível!"
 
     else:
         with open(html_file_path_boleto, 'r', encoding='utf-8') as file:
             html_body = file.read()
-            html_body = html_body.format(cliente=dados['cliente'], emissao=dados['emissao'], empresa=dados['empresa'], numeroNotaFiscal=dados['nota'], vencimentoNotaFiscal=dados['vencimento'], valorBoleto=dados['boleto'], valorTitulo=dados['titulo'])
+            html_body = html_body.format(cliente=dados['cliente'], emissao=dados['emissao'], empresa=dados['empresa'], numeroNotaFiscal=dados['titulo'], vencimentoNotaFiscal=dados['vencimento'], valorBoleto=dados['boleto'], valorTitulo=dados['nota'])
         assunto = f"Olá {dados['cliente']}, o boleto está disponível!"
+        email.Attachments.Add(dados['caminhoBoleto'])
 
     email.To = dados['email']
     email.Subject = assunto
     email.HTMLBody = html_body
+    email.Attachments.Add(dados['caminhoNota'])
 
     email.Send()
 

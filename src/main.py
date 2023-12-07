7af658f380e5a1ca84cb2db11b1ac1e1f44fe8db
guilherme rabelo <guilherme.rabelo@matriz.fornecedoramaquinas.com.br>
2023-12-07 15:45:29 -0300

    Feat/Criando funcao de mudanca da empresa

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from boleto import downloadBoleto
 from notaFiscal import downloadNotaFiscal
 from sql import sqlPool
 from email import envioDoEmail
+from mudarEmpresa import selecionarEmpresa
 import warnings
 import json
 
@@ -15,7 +16,7 @@ with open("../config/config.json", "r", encoding="utf-8") as file:
     dealernetModulo = sensitive_data['modulosDealernet']['ContasAReceber']
     executavel = dealernetModulo['executavel']
     nomeModulo = dealernetModulo['nome']
-    
+
 def tratarErro(funcao, mensagem, param1, param2):
     try:
         funcao(param1, param2)
@@ -23,36 +24,51 @@ def tratarErro(funcao, mensagem, param1, param2):
         print(f'Erro ao "{mensagem}"')
 
 tratarErro(loginDealernet, 'efetuar login', executavel, senha)
+empresas = sqlPool("""
+                    SELECT 
+                        emp_cd,
+                        emp_ds,
+                        emp_banco
+                    FROM [BD_MTZ_FOR]..ger_emp
+                    WHERE 
+                        emp_cd IN ('01')
+                        --emp_cd NOT IN ('20', '10', '07', '06', '05', '08', '09')
+                    ORDER BY emp_ds
+                """)
 
-titulosTotais = sqlPool("EXEC autocob.consulta_titulos '01'")
-def emailsNaoEnviados(titulo):
-    return titulo[14] == '1'
+for empresa in empresas:
+    codEmpresa = empresa[0]
+    empresa = empresa[1]
 
-titulosPendentes = list(filter(emailsNaoEnviados, titulosTotais))
-for i, titulo in enumerate(titulosPendentes):
-    numeroNota = titulo[7]
-    lancamento = titulo[5]
-    serie = titulo[8]
-    tipo = 'carteira' if titulo[4][0:2] == 'C.' else 'boleto'
-    dados = {
-        'cliente': titulo[3],
-        'email': 'guilherme.rabelo@grupofornecedora.com.br',
-        'emissao': titulo[11],
-        'vencimento': titulo[12],
-        'empresa': titulo[1],
-        'nota': titulo[10],
-        'boleto': titulo[9],
-        'titulo': titulo[5],
-        'caminhoNota': f'C:\\Users\\guilherme.rabelo\\Desktop\\TesteRPA\\NotasFiscais\\NF_{numeroNota}.pdf',
-        'caminhoBoleto': f'C:\\Users\\guilherme.rabelo\\Desktop\\TesteRPA\\Boletos\\Boleto_{lancamento}.pdf'
-    }
+    selecionarEmpresa(empresa)
 
-    if titulo[6] == '001':
-        downloadNotaFiscal(numeroNota, serie, tipo)
-    if tipo=='boleto':
-        downloadBoleto(nomeModulo, lancamento)
+    titulosTotais = sqlPool(f"EXEC autocob.consulta_titulos '{codEmpresa}'")
+    def emailsNaoEnviados(titulo):
+        return titulo[14] == '1'
 
-    envioDoEmail(tipo, dados)
-    if i==2:
-        break
+    titulosPendentes = list(filter(emailsNaoEnviados, titulosTotais))
+    for i, titulo in enumerate(titulosPendentes):
+        numeroNota = titulo[7]
+        lancamento = titulo[5]
+        serie = titulo[8]
+        tipo = 'carteira' if titulo[4][0:2] == 'C.' else 'boleto'
+        dados = {
+            'cliente': titulo[3],
+            'email': 'guilherme.rabelo@grupofornecedora.com.br',
+            'emissao': titulo[11],
+            'vencimento': titulo[12],
+            'empresa': titulo[1],
+            'nota': titulo[10],
+            'boleto': titulo[9],
+            'titulo': titulo[5],
+            'caminhoNota': f'C:\\Users\\guilherme.rabelo\\Desktop\\TesteRPA\\NotasFiscais\\NF_{numeroNota}.pdf',
+            'caminhoBoleto': f'C:\\Users\\guilherme.rabelo\\Desktop\\TesteRPA\\Boletos\\Boleto_{lancamento}.pdf'
+        }
+
+        if titulo[6] == '001':
+            downloadNotaFiscal(numeroNota, serie, tipo)
+        if tipo=='boleto':
+            downloadBoleto(nomeModulo, lancamento)
+
+        envioDoEmail(tipo, dados)
 
---
+++ src/mudarEmpresa.py
@@ -0,0 +1,44 @@
+from pywinauto.application import Application
+import time
+import pyautogui
+
+def selecionarEmpresa(empresa):
+    print('iniciando mudança para a empresa: ', empresa)
+    app = Application(backend="win32").connect(class_name="FNWND3115")
+    main_window = app.top_window()
+    main_window.set_focus()
+    time.sleep(5)
+
+    pyautogui.hotkey('alt', 'p')
+    time.sleep(.5)
+    for i in range(4):
+        pyautogui.press('up')
+        time.sleep(.01)
+    time.sleep(1)
+    pyautogui.press('ENTER')
+    time.sleep(5)
+
+    telaEmpresa = Application(backend="win32").connect(title='Segurança')
+    time.sleep(.5)
+    telaEmpresa.Seguranca.set_focus()
+    time.sleep(2)
+    empresaEncontrada = False
+    cont = 0
+    while empresaEncontrada==False:
+        empresaSelecionada = telaEmpresa.Seguranca.children()[2].window_text()
+        empresaFormatada = empresaSelecionada[:-2].strip()
+
+        if empresaFormatada==empresa:
+            empresaEncontrada = True
+        else:
+            if cont <= 25:
+                pyautogui.press('down')
+            else: 
+                pyautogui.press('up')
+        time.sleep(.5)
+        cont+=1
+        if cont == 50:
+            break
+    telaEmpresa.Seguranca.child_window(title="&OK", class_name="Button").wrapper_object().click_input()
+    time.sleep(5)
+                      
